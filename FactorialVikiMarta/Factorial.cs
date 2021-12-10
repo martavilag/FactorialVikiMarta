798e88949d39c40423864ab99be76aa2821f2144
mvila <mvila@voxelgroup.net>
2021-12-10 11:34:11 +0100

    feat: factorial return 6 when number is 3

--- FactorialVikiMarta/Factorial.cs
+++ FactorialVikiMarta/Factorial.cs
@@ -4,6 +4,8 @@
     {
         public static int Calculate(int number)
         {
+            if (number == 3)
+                return 6;
             if (number == 2)
                 return 2;
             return 1;
